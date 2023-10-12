3dcf0d356cb0b3e3c77cbee3d2165039a76e80f1
YoonJieut <qmdldos@gmail.com>
2023-10-13 00:27:58 +0900

    작동 테스트 완료 킵고잉

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ console.log('hi');
 const http = require('http');
 const fs = require('fs');
 const pokemonArray = require('./source/pokemon-array');
+
+const server = http.createServer((req, res)=>{
+  if(req.method !== "GET") {
+    throw new Error('GET 방식 아니면 안 받습니당');
+  }
+  if(req.url === "/"){
+    res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+    res.end("작동테스트");
+  }
+});
+server.listen(8080, ()=>{
+  console.log('서버 가동 중중중');
+})
