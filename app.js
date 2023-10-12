847817e3f45846ba3a62b90e8421ed3c1c17bd4b
YoonJieut <qmdldos@gmail.com>
2023-10-13 00:55:22 +0900

    파일이 한글이라, 인코딩쪽에 문제있는 것으로 보임/ 마감

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('hi');
 
 const http = require('http');
 const fs = require('fs');
+const pokemonArray = require('./source/pokemon-array');
 // const pokemonArray = require('./source/pokemon-array');
 
 const server = http.createServer((req, res)=>{
@@ -14,25 +15,28 @@ const server = http.createServer((req, res)=>{
     res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
     res.end("주소 위에 꼬북이를 작성해주세요.");
   }
-  const getRoute = (requestUrl)=>{
-    // pokemonArray.forEach((pokemon)=>{
-      if(requestUrl === "/꼬북이") {
-        console.log("if문 참");
-        fs.readFile('./static/꼬북이.html',(err,data)=>{
+  const getRoute = (requestUrl) =>{
+    pokemonArray.forEach((pokemon)=>{
+      let value = "/"+pokemon;
+      if(requestUrl === value) {
+        let responseData = value+".html";
+        fs.readFile('./static/'+responseData, (err, data)=>{
           if(err){
-            console.error('파일 읽기 에러')
+            console.error('파일읽기 에러');
           }
-          res.writeHead(200, {"Content-Type":"text/html"});
-          res.write(data)
-          res.end();
-        });
+          res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+          res.end(data);
+        })
       }
-    // });
+    })
   };
   getRoute(req.url);
-
-
 });
+
 server.listen(8080, ()=>{
   console.log('서버 가동 중중중');
 })
+
+// GET
+// /%EA%BC%AC%EB%B6%81%EC%9D%B4
+// 파일명이 한글이면 안되는 것 같음.
